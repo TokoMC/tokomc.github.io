a5d87c882650d50c8eda0262a6383351a559e60d
TokoMC <poryazx1389@gmail.com>
2026-07-27 08:34:39 +0330

    Create ranks.js

---
+++ data/ranks.js
@@ -0,0 +1,42 @@
+export const ranks = [
+  {
+    name: "PLAYER",
+    level: 0,
+  },
+  {
+    name: "VIP",
+    level: 1,
+  },
+  {
+    name: "VIP+",
+    level: 2,
+  },
+  {
+    name: "LEGEND",
+    level: 3,
+  },
+  {
+    name: "MVP",
+    level: 4,
+  },
+  {
+    name: "HELPER",
+    level: 5,
+  },
+  {
+    name: "MODERATOR",
+    level: 6,
+  },
+  {
+    name: "ADMIN",
+    level: 7,
+  },
+  {
+    name: "OWNER",
+    level: 8,
+  },
+  {
+    name: "MAIN_OWNER",
+    level: 9,
+  },
+];
