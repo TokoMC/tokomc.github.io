3d633075c62e9457385ff7f599f43697b7fc34ce
TokoMC <poryazx1389@gmail.com>
2026-07-27 08:24:45 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import ServerStatus from "@/components/ServerStatus";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+
       <Hero />
+
+      <ServerStatus />
     </main>
   );
 }
