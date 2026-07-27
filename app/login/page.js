d2ebcfd2a3e12c709f812b088e3e83292801b5b4
TokoMC <poryazx1389@gmail.com>
2026-07-27 09:44:42 +0330

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+
+
 export default function LoginPage() {
+
+  const router = useRouter();
+
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [message, setMessage] = useState("");
+
+
+
+  async function handleLogin(e) {
+
+    e.preventDefault();
+
+
+    setMessage("در حال ورود...");
+
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+
+      email: email,
+
+      password: password,
+
+    });
+
+
+
+    if (error) {
+
+      setMessage(error.message);
+
+      return;
+
+    }
+
+
+
+    setMessage("ورود موفق بود");
+
+
+    router.push("/profile");
+
+
+  }
+
+
+
   return (
+
     <main
       className="
       min-h-screen
@@ -10,7 +67,11 @@ export default function LoginPage() {
       "
     >
 
-      <div
+
+      <form
+
+        onSubmit={handleLogin}
+
         className="
         w-full
         max-w-md
@@ -21,9 +82,12 @@ export default function LoginPage() {
         rounded-3xl
         p-8
         "
+
       >
 
+
         <h1
+
           className="
           text-3xl
           font-black
@@ -31,50 +95,75 @@ export default function LoginPage() {
           text-center
           mb-8
           "
+
         >
+
           ورود به TokoMC
+
         </h1>
+
 
 
         <div className="space-y-5">
 
+
+
           <input
+
             type="email"
+
             placeholder="ایمیل"
+
+            value={email}
+
+            onChange={(e)=>setEmail(e.target.value)}
+
             className="
             w-full
             px-5
             py-3
             rounded-xl
             bg-black/30
+            text-white
             border
             border-white/10
-            text-white
-            outline-none
-            focus:border-green-400
             "
+
           />
+
+
 
 
           <input
+
             type="password"
+
             placeholder="رمز عبور"
+
+            value={password}
+
+            onChange={(e)=>setPassword(e.target.value)}
+
             className="
             w-full
             px-5
             py-3
             rounded-xl
             bg-black/30
+            text-white
             border
             border-white/10
-            text-white
-            outline-none
-            focus:border-green-400
             "
+
           />
+
+
 
 
           <button
+
+            type="submit"
+
             className="
             w-full
             py-3
@@ -85,15 +174,33 @@ export default function LoginPage() {
             hover:scale-105
             transition
             "
+
           >
+
             ورود
+
           </button>
+
+
+
+          <p className="text-green-400 text-center">
+
+            {message}
+
+          </p>
+
+
 
         </div>
 
 
-      </div>
+
+      </form>
+
+
 
     </main>
+
   );
+
 }
