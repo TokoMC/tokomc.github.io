53964fbc9aed1a45b495cf05fba8d6ecdd4e54f1
TokoMC <poryazx1389@gmail.com>
2026-07-27 08:28:58 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Hero from "@/components/Hero";
 import ServerStatus from "@/components/ServerStatus";
 import Features from "@/components/Features";
 import Ranks from "@/components/Ranks";
+import Rules from "@/components/Rules";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -17,6 +19,10 @@ export default function Home() {
       <Features />
 
       <Ranks />
+
+      <Rules />
+
+      <Footer />
 
     </main>
   );
